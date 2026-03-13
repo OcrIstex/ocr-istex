712cb3a73018c61f5a20a9eeef75c072dd38db40
Mathis EON <mathis.eon@inist.fr>
2026-03-13 11:50:06 +0100

    Fix s3 url

--- init.sh
+++ init.sh
@@ -7,7 +7,9 @@ chown -R onyxia:users ${GIT_REPO}/
 REQUIREMENTS_FILE=${GIT_REPO}/requirements.txt
 [ -f $REQUIREMENTS_FILE ] && pip install -r $REQUIREMENTS_FILE
 
-mc cp s3/eonm/istex-search-metadata-small.db ./istex-search-metadata-small.db
+
+mkdir -p data
+mc cp s3/eonm/diffusion/istex-search-metadata-small.db ./data/istex-search-metadata-small.db
 
 # Open the relevant notebook when starting Jupyter Lab
 # echo "c.LabApp.default_url = '/lab/tree/${GIT_REPO}/OCR dans Istex.ipynb'" >> /home/onyxia/.jupyter/jupyter_server_config.py
