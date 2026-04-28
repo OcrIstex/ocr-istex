fb1b4b73535991ab2dcf6b4a7f5ead00c36b4c74
Mathis EON <60341438+eonm-pro@users.noreply.github.com>
2026-04-28 13:41:58 +0200

    Enhance init.sh with DuckDB integration
    Added DuckDB commands to attach databases and create a new table from joined data.

--- init.sh
+++ init.sh
@@ -10,5 +10,18 @@ REQUIREMENTS_FILE=${GIT_REPO}/requirements.txt
 mkdir -p data
 
 mc cp $ISTEX_SEARCH_METADATA ./data/istex-search-metadata.db
-mc cp $OCR_FULLTEXT_DATA ./data/ocr-fulltext.db
-mc cp $OCR_DETECTOR_DATA ./data/ocr-detector.db
+mc cp $OCR_FULLTEXT_DATA     ./data/ocr-fulltext.db
+mc cp $OCR_DETECTOR_DATA     ./data/ocr-detector.db
+
+duckdb ./data/output.db <<'SQL'
+ATTACH '/home/onyxia/work/data/ocr-detector.db'          AS ocr_detector (TYPE SQLITE);
+ATTACH '/home/onyxia/work/data/istex-search-metadata.db' AS metadata;
+
+CREATE TABLE data AS (
+    SELECT
+        DECADE(MAKE_DATE(publicationDate::INT)) AS decade,
+        *
+    FROM ocr_detector.documents
+    JOIN metadata.records ON ark = arkIstex
+);
+SQL
